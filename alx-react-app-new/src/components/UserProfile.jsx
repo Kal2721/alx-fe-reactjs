56cfa33b9125812b3d9e42a7949d54ab305ab019
kalkidan <kalkidanteshale6@gmail.com>
2025-07-17 17:38:04 +0300

    Correcting the style of the header

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 const UserProfile = (props) => {
    return (
-     <div style={{ border: '0.5px solid gray', padding: '10px', margin: '10px'}}>
-	   <h2 style={{color: 'gray'}}><span style={{ fontWeight: 'bold'}}>{props.name}</span></h2>
+     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px'}}>
+	   <h2 style={{ color: 'gray' }>{props.name}</h2>
 	   <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
 	   <p>Bio: <span style={{ fontSize: 'l.275rem'}}>{props.bio}</span></p>
      </div>
